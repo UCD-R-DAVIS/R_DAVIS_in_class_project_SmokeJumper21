e4c556af77acf8aed4630b3e351ea9eefcad45d0
Christopher Nguyen <christophernguyen@Christophers-MacBook-Pro.local>
2024-10-18 17:35:23 -0700

    In class.

---
+++ Scripts/Week_IntrotoR.R
@@ -0,0 +1,14 @@
+#Challenge
+
+elephant1_kg <- 3492
+elephant2_lb <- 7757
+
+elephant1_lb <- elephant1_kg *2.2
+elephant2_lb > elephant1_lb
+
+myelephants <- c(elephant1_lb, elephant2_lb)
+
+which(myelephants ==max(myelephants))
+
+weight_g <-(50,60,65,82)
+treatment <- factor(c("high", "low", "low", "medium", "high"))
