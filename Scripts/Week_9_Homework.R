a0f97f0b52f3509c4d6328e38a00767fd100afa8
Christopher Nguyen <cchnguyen@ucdavis.edu>
2024-11-21 13:49:53 -0800

    Week 9 HW Complete.

---
+++ Scripts/Week_9_Homework.R
@@ -0,0 +1,14 @@
+for(i in unique(surveys$taxa)){
+  mytaxon <- surveys[surveys$taxa == i,]
+  longestnames <- mytaxon[nchar(mytaxon$species) == max(nchar(mytaxon$species)),] %>% select(species)
+  print(paste0("The longest species name(s) among ", i, "s is/are: "))
+  print(unique(longestnames$species))
+}
+mycols <- mloa %>% select("windDir","windSpeed_m_s","baro_hPa","temp_C_2m","temp_C_10m","temp_C_towertop","rel_humid", "precip_intens_mm_hr")
+mycols %>% map(max, na.rm = T)
+C_to_F <- function(x){
+  x * 1.8 + 32
+}
+mloa$temp_F_2m <- C_to_F(mloa$temp_C_2m)
+mloa$temp_F_10m <- C_to_F(mloa$temp_C_10m)
+mloa$temp_F_towertop <- C_to_F(mloa$temp_C_towertop)
