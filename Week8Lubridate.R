924f9817cbc6a8ec4332e71345061905c96ac382
Christopher Nguyen <cchnguyen@ucdavis.edu>
2024-11-21 13:47:24 -0800

    Wrong folder.

--- Week8Lubridate.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-library(lubridate)
-mloa <- read_csv("https://raw.githubusercontent.com/ucd-r-davis/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv")
-head(mloa)
-mloa2 <- mloa %>%
-  filter(rel_humid != -99) %>%  # Remove missing humidity values
-  filter(temp_C_2m != -999.9) %>%  # Remove missing temperature values
-  filter(windSpeed_m_s != -999.9)  # Remove missing wind speed values
-colnames(mloa)
